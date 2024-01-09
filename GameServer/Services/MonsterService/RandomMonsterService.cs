62cb43455de66bbf131a61c29960161ea204d996
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2024-01-09 15:14:09 +0300

    finish

--- GameServer/Services/MonsterService/RandomMonsterService.cs
+++ GameServer/Services/MonsterService/RandomMonsterService.cs
@@ -7,9 +7,9 @@ public class RandomMonsterService(AppDbContext context) : IRandomMonsterService
 {
     public Monster GetRandomMonster()
     {
-        IEnumerable<Monster> monsters = context.Monsters!;
+        IQueryable<Monster> monsters = context.Monsters!;
         var rnd = new Random();
-        var id = rnd.Next(1,monsters.Count()+1);
-        return monsters.First(m => m.Id == id);
+        var id = rnd.Next(0,monsters.Count());
+        return monsters.FirstOrDefault(x => x.Id == id)!;
     }
 }
