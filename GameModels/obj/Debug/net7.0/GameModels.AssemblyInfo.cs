1731649fd651fc39ec88cc4ddf509fa73fa791fd
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2024-01-09 13:47:39 +0300

    refactoring

--- GameModels/obj/Debug/net7.0/GameModels.AssemblyInfo.cs
+++ GameModels/obj/Debug/net7.0/GameModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8934cc8fed8524e6c6659ce2d7becd0fcc7809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baff15aa8b4f51a0564cb20699009aa41865b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
